706970b18dfce0473d7655c6c571aea446b13617
stealcash <pawansaini312@gmail.com>
2025-04-04 21:51:06 +0530

    allow method to support n number also support new line breakup for number

--- src/utility/add.test.ts
+++ src/utility/add.test.ts
@@ -7,4 +7,10 @@ describe("string calculator - test cases of add function", () => {
     test("check for sum of two numbers seperate by comma", () => {
         expect(add("1,2")).toBe(3);
     });
+    test("should handle addition of n numbers", () => {
+        expect(add("1,2,3,4,5,6,7,8,9,0")).toBe(45);
+    });
+    test("should handle new line as a delimiter", () => {
+        expect(add("1\n2,3\n4")).toBe(10);
+    });
 });
